e7a9df80891125321aad2f4f9fc45df717276616
JoseAlfaroCC <josalfaro@coca-cola.com>
2025-04-16 15:30:34 +0000

    First Challenge

--- src/model/train.py
+++ src/model/train.py
@@ -4,24 +4,32 @@ import argparse
 import glob
 import os
 
+import mlflow
+
+import numpy as np
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
 # define functions
 def main(args):
     # TO DO: enable autologging
+        # Enable autologging
+    mlflow.autolog()
 
+    # Start an MLflow run
+    with mlflow.start_run():
 
-    # read data
-    df = get_csvs_df(args.training_data)
+        # read data
+        df = get_csvs_df(args.training_data)
 
-    # split data
-    X_train, X_test, y_train, y_test = split_data(df)
+        # split data
+        X_train, X_test, y_train, y_test = split_data(df)
 
-    # train model
-    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+        # train model
+        train_model(args.reg_rate, X_train, X_test, y_train, y_test)
 
 
 def get_csvs_df(path):
@@ -34,7 +42,10 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
-
+def split_data(df):
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    return X_train, X_test, y_train, y_test  
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
